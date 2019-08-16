97a8205fa7f8d8fa83331acc99cbdcec0750a815
Alex Beregszaszi <alex@rtfs.hu>
2019-08-16 17:14:40 +0200

    Add Berlin HF

--- test/unittests/test_instructions.cpp
+++ test/unittests/test_instructions.cpp
@@ -255,3 +255,17 @@ TEST(instructions, istanbul_hard_fork)
         EXPECT_STREQ(in[op], pn[op]) << op;
     }
 }
+
+TEST(instructions, berlin_hard_fork)
+{
+    const auto b = evmc_get_instruction_metrics_table(EVMC_BERLIN);
+    const auto i = evmc_get_instruction_metrics_table(EVMC_ISTANBUL);
+    const auto bn = evmc_get_instruction_names_table(EVMC_BERLIN);
+    const auto in = evmc_get_instruction_names_table(EVMC_ISTANBUL);
+
+    for (int op{OP_STOP}; op <= OP_SELFDESTRUCT; ++op)
+    {
+        EXPECT_EQ(b[op], i[op]) << op;
+        EXPECT_STREQ(bn[op], in[op]) << op;
+    }
+}
